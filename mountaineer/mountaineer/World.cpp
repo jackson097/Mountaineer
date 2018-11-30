0bebddb363a1b288878f9a3409689e707899a863
Jackson Newlands <jackson_newlands@live.ca>
2018-11-30 00:27:52 -0500

    Made objects smaller

--- mountaineer/mountaineer/World.cpp
+++ mountaineer/mountaineer/World.cpp
@@ -8,13 +8,13 @@ World::World() {
 	objnum = 3; // number of object
 	list[0] = (Shape*) malloc( sizeof(Shape*) * objnum);  // array to hold object pointers
 	list[0] = new Character();
-	list[0]->scaleChange(-0.3);
+	list[0]->scaleChange(-0.6);
 	list[1] = new Cube();
 	list[1]->translate(-3,0,0);
-	list[1]->scaleChange(-0.3);
+	list[1]->scaleChange(-0.6);
 	list[2] = new Cube();
 	list[2]->translate(3,0,0);
-	list[2]->scaleChange(-0.3);
+	list[2]->scaleChange(-0.6);
 
 }
 
